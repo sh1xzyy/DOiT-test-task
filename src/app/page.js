d1f9712b593e449ca8411d07c1bfcb1b488f3d97
sh1xzyy <alekslungol@gmail.com>
2025-08-26 15:44:57 +0300

    hero logic and styles

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,12 @@
+import Hero from '@/components/Hero/Hero'
+import css from './page.module.css'
+
 const Home = () => {
-	return <div>Home Page</div>
+	return (
+		<section className={css.section}>
+			<Hero />
+		</section>
+	)
 }
 
 export default Home
